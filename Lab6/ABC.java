3cf1fe11d55d819a14c0a5a7e1f360a4e2070922
saada865 <80025562+saada865@users.noreply.github.com>
2021-04-12 18:05:39 +0500

    Add files via upload

---
+++ Lab6/ABC.java
@@ -0,0 +1,19 @@
+package l6ac3;
+
+public class ABC {
+     
+    public static int i ;
+    public String s;
+
+ public static void displayStatic()
+ {
+    System.out.println("i:"+i);
+ }
+ 
+ public void display() 
+ {
+ System.out.println("i:"+i);
+ System.out.println("s:"+s);
+ }
+
+}
---
+++ Lab6/Calculator.java
@@ -0,0 +1,42 @@
+package l6as1;
+
+public class Calculator {
+    
+    public static double sum(double a, double b){
+        
+        return (a+b);
+    }
+    
+    public static double multiply(double a, double b){
+        
+        return (a*b);
+    }
+    
+    public static double divide(double a, double b){
+        
+        return (a/b);
+    }
+    
+    public static double modulus(double a, double b){
+        
+        return (a%b);
+    } 
+    
+    public static double sin(double a){
+        
+        return(Math.sin(a));
+    } 
+    
+    public static double cos(double a){
+        
+        return(Math.cos(a));
+    }
+    
+    public static double tan(double a){
+        
+        return(Math.tan(a));
+    }
+    
+    
+    
+}
